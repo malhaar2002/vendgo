6ce6dd1c11835e5d8b5097267873ccee2fe95bd8
Malhaar Arora <58373047+malhaar2002@users.noreply.github.com>
2022-05-29 20:50:59 +0530

    Delete vendor directory

--- vendor/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from vendor import views
-
-urlpatterns = [
-    path('', views.vendor_dashboard, name='vendor_dashboard'),
-    path('view_order', views.view_order, name='view_order'),
-    path('view_catalogue', views.view_catalogue, name='view_catalogue'),
-    path('add_band', views.add_band, name='add_band')
-]
